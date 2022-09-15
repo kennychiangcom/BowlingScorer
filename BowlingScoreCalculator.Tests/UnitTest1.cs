2e2979a9e5ff3c530dc2b8e47fbb152552a03aa3
kennychiangcom <kenny@chiang.com>
2022-09-15 23:41:50 +0100

    Test cases of all strikes and no strikes/spares are passed

--- BowlingScoreCalculator.Tests/UnitTest1.cs
+++ BowlingScoreCalculator.Tests/UnitTest1.cs
@@ -15,6 +15,13 @@ public class Tests
     [Test]
     public void Test1()
     {
+        //                                                                   BONUS_FINAL_ROLLS------v
+        //                                                                            MAX_ROLL--v
+        //Frame#    1       2       3       4       5       6       7       8       9       10		
+        //Roll#     1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21
+        //rS        0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20
+        //rollScore 30  0   30  0   30  0   30  0   30  0   30  0   30  0   30  0   30  0   30  20  10
+
         // (When scoring “X” indicates a strike, “/” indicates a spare, “-” indicates a miss)
 
         // X X X X X X X X X X X X(12 rolls: 12 strikes) = 10 frames * 30 points = 300
@@ -24,12 +31,12 @@ public class Tests
     public void Test2()
     {
         // 9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 - (20 rolls: 10 pairs of 9 and miss) = 10 frames * 9 points = 90
-        BowlingScoreCalculator.ScoreCalc("9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 -").Should().Be(300);
+        BowlingScoreCalculator.ScoreCalc("9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 -").Should().Be(90);
     }
     [Test]
     public void Test3()
     {
         // 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5(21 rolls: 10 pairs of 5 and spare, with a final 5) = 10 frames * 15 points = 150
-        BowlingScoreCalculator.ScoreCalc("5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5").Should().Be(300);
+        BowlingScoreCalculator.ScoreCalc("5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5").Should().Be(150);
     }
 }
--- BowlingScoreCalculator/Program.cs
+++ BowlingScoreCalculator/Program.cs
@@ -3,29 +3,56 @@
 public class BowlingScoreCalculator
 {
     public static void Main() { }
-    public static int ScoreCalc(string scoreFrames)
+    public static int ScoreCalc(string scoreRolls)
     {
         const int ZERO_BASED_ADJ = -1;
-        const int MAX_FRAME = 10;
-        const int BONUS_FINAL_ROLLS = 2;
-        int[] frameScore = new int[MAX_FRAME + BONUS_FINAL_ROLLS];
-        string[] Frames = scoreFrames.Split(' ');
-        for (int i = 0; i <= Frames.Length + ZERO_BASED_ADJ; i++)
+        const int MAX_ROLL = 20;
+        const int BONUS_FINAL_ROLLS = 1;
+        int[] rollScore = new int[MAX_ROLL + BONUS_FINAL_ROLLS];
+        int rS = 0;
+        string[] Rolls = scoreRolls.Split(' ');
+
+        for (int Roll = 0; Roll < Rolls.Length; Roll++)
         {
             //debug code
-            for (int Frame = 0; Frame <= frameScore.Length + ZERO_BASED_ADJ; Frame++) {
-                Console.Write(frameScore[Frame]);
+            for (int dbgRoll = 0; dbgRoll < Rolls.Length; dbgRoll++)
+            {
+                Console.Write(Rolls[dbgRoll]);
                 Console.Write(' ');
             }
             Console.Write('\r');
+            for (int dbgRoll = 0; dbgRoll < rollScore.Length; dbgRoll++)
+            {
+                Console.Write(rollScore[dbgRoll]);
+                Console.Write(' ');
+            }
+            Console.Write('\r');
+            Console.Write("Roll:{0}   rS:{1}\r",Roll,rS);
             //end debug code
 
-            switch (Frames[i])
+            switch (Rolls[Roll])
             {
                 case "X":
-                    frameScore[i] = 10;
-                    if (i - 1 >= 0 && Frames[i - 1].Equals("X")) { frameScore[i - 1] += 10; }
-                    if (i - 2 >= 0 && Frames[i - 2].Equals("X")) { frameScore[i - 2] += 10; }
+                    rollScore[rS] = 10;
+                    if (rS < MAX_ROLL-1)
+                    {
+                        if (Roll - 1 >= 0 && Rolls[Roll - 1].Equals("X")) { rollScore[rS - 2] += 10; }
+                        if (Roll - 2 >= 0 && Rolls[Roll - 2].Equals("X")) { rollScore[rS - 4] += 10; }
+                        if (rS < MAX_ROLL-2) rS++;
+                    }
+                    else if(rS < MAX_ROLL)
+                    {
+                        if (Rolls[Roll - 1].Equals("X")) { rollScore[rS - 1] += 10; }
+                        if (Rolls[Roll - 2].Equals("X")) { rollScore[rS - 3] += 10; }
+                    }
+                    else if (rS < MAX_ROLL + BONUS_FINAL_ROLLS)
+                    {
+                        if (Rolls[Roll - 1].Equals("X")) { rollScore[rS - 1] += 10; }
+                        if (Rolls[Roll - 2].Equals("X")) { rollScore[rS - 2] += 10; }
+                    }
+                    break;
+                case "0": case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9":
+                    rollScore[rS] = int.Parse(Rolls[Roll]);
                     break;
                 case "/":
                     break;
@@ -33,7 +60,18 @@ public class BowlingScoreCalculator
                     break;
                 //default:
             }
+            rS++;
+
+            //debug code
+            for (int dbgRoll = 0; dbgRoll < rollScore.Length; dbgRoll++)
+            {
+                Console.Write(rollScore[dbgRoll]);
+                Console.Write(' ');
+            }
+            Console.Write("\r\r");
+            //end debug code
+
         }
-        return frameScore.Sum() - frameScore[10] - frameScore[11]; 
+        return rollScore.Sum() - rollScore[19] - rollScore[20]; 
     }
 }
