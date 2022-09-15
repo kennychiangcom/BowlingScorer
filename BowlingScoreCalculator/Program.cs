796e9c54fc1c3d24cfeb4f802daf6751657e511e
kennychiangcom <kenny@chiang.com>
2022-09-15 02:56:21 +0100

    Made one test case passed

--- BowlingScoreCalculator/Program.cs
+++ BowlingScoreCalculator/Program.cs
@@ -6,21 +6,26 @@ public class BowlingScoreCalculator
     public static int ScoreCalc(string scoreFrames)
     {
         const int ZERO_BASED_ADJ = -1;
-        int[] frameScore = new int[12];
+        const int MAX_FRAME = 10;
+        const int BONUS_FINAL_ROLLS = 2;
+        int[] frameScore = new int[MAX_FRAME + BONUS_FINAL_ROLLS];
         string[] Frames = scoreFrames.Split(' ');
         for (int i = 0; i <= Frames.Length + ZERO_BASED_ADJ; i++)
         {
+            //debug code
+            for (int Frame = 0; Frame <= frameScore.Length + ZERO_BASED_ADJ; Frame++) {
+                Console.Write(frameScore[Frame]);
+                Console.Write(' ');
+            }
+            Console.Write('\r');
+            //end debug code
+
             switch (Frames[i])
             {
                 case "X":
                     frameScore[i] = 10;
-                    if (i - 1 >= 0 && frameScore[i - 1] == 'X') { frameScore[i - 1] += 10; }
-                    if (i - 2 >= 0 && frameScore[i - 2] == 'X') { frameScore[i - 2] += 10; }
-                    foreach (int Frame in frameScore) {
-                        Console.Write(frameScore[Frame]);
-                        Console.Write(' ');
-                    }
-                    Console.Write('\r');
+                    if (i - 1 >= 0 && Frames[i - 1].Equals("X")) { frameScore[i - 1] += 10; }
+                    if (i - 2 >= 0 && Frames[i - 2].Equals("X")) { frameScore[i - 2] += 10; }
                     break;
                 case "/":
                     break;
@@ -29,6 +34,6 @@ public class BowlingScoreCalculator
                 //default:
             }
         }
-        return frameScore.Sum(); 
+        return frameScore.Sum() - frameScore[10] - frameScore[11]; 
     }
 }
